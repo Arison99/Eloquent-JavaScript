991c3186185869893e55ec1c1489d50465c8346a
Arison99 <harrisondaviinci@gmail.com>
2025-01-15 11:13:05 +0300

    Implement Breadth-First Search (BFS) algorithm with example usage

--- Algorithms/Breadth-First Search (BFS).js
+++ Algorithms/Breadth-First Search (BFS).js
@@ -1,12 +1,37 @@
+// Breadth-First Search (BFS) algorithm implementation
 function bfs(graph, start) {
+    // Set to keep track of visited nodes
     let visited = new Set();
+    // Queue to manage the nodes to be explored
     let queue = [start];
+    
+    // Loop until there are nodes to be explored
     while (queue.length > 0) {
+        // Dequeue the first node
         let node = queue.shift();
+        
+        // If the node has not been visited
         if (!visited.has(node)) {
+            // Process the node (e.g., print it)
             console.log(node);
+            // Mark the node as visited
             visited.add(node);
+            // Enqueue all adjacent nodes
             queue.push(...graph[node]);
         }
     }
 }
+
+// Example usage
+// Define a graph as an adjacency list
+const graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+};
+
+// Perform BFS starting from node 'A'
+bfs(graph, 'A');
